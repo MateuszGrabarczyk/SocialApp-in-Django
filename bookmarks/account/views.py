f180aa14e35f292a14d2c71fba3dec01fd610a01
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-03-10 08:59:23 +0100

    Added login and authentication to the app

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authentication process succeded')
+                else:
+                    return HttpResponse('Account is blocked')
+            else:
+                return HttpResponse('Incorrect data.')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
