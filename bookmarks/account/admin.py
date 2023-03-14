b0b7f12445bb4ef580fde4b17c50b22d0a5bce61
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-03-14 10:48:31 +0100

    Profile added to the system

--- bookmarks/account/admin.py
+++ bookmarks/account/admin.py
@@ -1,3 +1,10 @@
+from .models import Profile
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    '''Admin View for Profile'''
+
+    list_display = ['user', 'date_of_birth', 'photo']
+    
